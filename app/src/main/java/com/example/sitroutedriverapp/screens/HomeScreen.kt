18a0b4a9dda13ba77f5e79a2f23573960fd2b223
EgorLatop <egor.antipin.1995mail.ru@gmail.com>
2023-12-05 14:16:51 +0300

    navigation

--- app/src/main/java/com/example/sitroutedriverapp/screens/HomeScreen.kt
+++ app/src/main/java/com/example/sitroutedriverapp/screens/HomeScreen.kt
@@ -1,30 +1,48 @@
 package com.example.sitroutedriverapp.screens
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.sitroutedriverapp.R
 import com.example.sitroutedriverapp.Routes
 import com.example.sitroutedriverapp.component.CustomTopAppBar
 
 @Composable
-fun HomeScreen
-            (navController: NavHostController) {
-    Box(modifier = Modifier.fillMaxSize()) {
-        ScaffoldWithTopBar(navController)
+fun HomeScreen(navController: NavHostController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        DrawerHeader()
+//"сдесь инва по автобусу зарядке и кнопка чата",
     }
 }
 
@@ -33,16 +51,16 @@ fun HomeScreen
 @Composable
 fun ScaffoldWithTopBar(navController: NavHostController) {
     Scaffold(
-        topBar = {
-            CustomTopAppBar(navController, "Signup", true)
-        },
+        //topBar = {
+        //    CustomTopAppBar(navController, "Signup", true)
+        //},
         content = {
             Column(
                 modifier = Modifier
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background,)
             ) {
+                DrawerHeader()
                 Text(
                     text = "сдесь инва по автобусу зарядке и кнопка чата",
                     fontSize = 30.sp,
@@ -51,4 +69,17 @@ fun ScaffoldWithTopBar(navController: NavHostController) {
             }
         }
     )
+}
+
+@Composable
+private fun DrawerHeader() {
+    Row(modifier = Modifier.padding(16.dp), verticalAlignment = Alignment.CenterVertically) {
+        Icon(
+            painter = painterResource(id = R.drawable.sitroute_modile),
+            contentDescription = null,
+            //tint = MaterialTheme.colorScheme.primaryContainer,
+            modifier = Modifier.height(50.dp)
+        )
+        Text(text = "Sitroute", fontSize = 28.sp)
+    }
 }
--- app/src/main/java/com/example/sitroutedriverapp/screens/loginScreen.kt
+++ app/src/main/java/com/example/sitroutedriverapp/screens/loginScreen.kt
@@ -1,16 +1,40 @@
 package com.example.sitroutedriverapp.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalProvider
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.Visibility
 import androidx.navigation.NavHostController
+import com.example.sitroutedriverapp.R
 import com.example.sitroutedriverapp.Routes
 import com.example.sitroutedriverapp.models.User
 import com.example.sitroutedriverapp.settingsConnection
@@ -23,38 +47,100 @@ import retrofit2.Response
 fun LoginScreen(navController: NavHostController) {
     var login by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
-    var errorMessage by rememberSaveable { mutableStateOf("") }
-    Column {
-        Text("Sitroute")
-        Text("Логин")
-        TextField(value = login, onValueChange = { login = it })
-        Text("Пароль")
-        TextField(
-            value = password,
-            onValueChange = { password = it },
-            supportingText = { Text(errorMessage) })
-        Button(onClick = { navController.navigate(Routes.Home.route)
-            //val repos = settingsConnection().currentUser(login, password)
-            //repos.enqueue(object : Callback<User> {
-            //    override fun onResponse(call: Call<User>, response: Response<User>) {
-            //        if (response.isSuccessful) {
-            //            val currentUser = response.body()
-                            //            if (currentUser?.idUser == 0) {
-            //                errorMessage = "* Неправильный логин или пароль"
-                                    //            } else if (currentUser?.driver != null) {
-            //                 navController.navigate(Routes.Home.route)
-                                    //            } else {
-            //                errorMessage = "У вас нет прав для этого приложения"
-                                    //            }
-                                //        }
-                        //    }
-//
-            //    override fun onFailure(call: Call<User>, t: Throwable) {
-            //        errorMessage = "Ошибка"
-                        //    }
-            //})
-        }) {
-            Text("Войти")
+    var errorMessage by remember { mutableStateOf("") }
+    var passwordVisible by rememberSaveable { mutableStateOf(false) }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.sitroute_modile),
+            contentDescription = null,
+            modifier = Modifier.padding(top = 80.dp).size(100.dp),
+
+        )
+        Text(
+            "Sitroute",
+            modifier = Modifier.padding(0.dp, 5.dp, 0.dp, 0.dp),
+            fontSize = 38.sp,
+        )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                "Логин",
+                modifier = Modifier.padding(0.dp, 10.dp),
+                fontSize = 28.sp,
+            )
+            TextField(value = login,
+                onValueChange = { login = it },
+                singleLine = true,)
+            Text(
+                "Пароль",
+                modifier = Modifier.padding(0.dp, 10.dp),
+                fontSize = 28.sp,
+            )
+//            TextField(
+//                value = password,
+//                onValueChange = { password = it },
+//                supportingText = { Text(errorMessage) },
+//                )
+
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                //label = { Text("Password") },
+                singleLine = true,
+                //placeholder = { Text("Password") },
+                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                supportingText = { Text(errorMessage) },
+                trailingIcon = {
+                    val image = if (passwordVisible)
+                        Icons.Filled.Visibility
+                    else Icons.Filled.VisibilityOff
+
+                    val description = if (passwordVisible) "Hide password" else "Show password"
+
+                    IconButton(onClick = {passwordVisible = !passwordVisible}){
+                        Icon(imageVector  = image, description)
+                    }
+                }
+            )
+
+
+            Button(modifier = Modifier.width(270.dp).padding(0.dp,20.dp,0.dp,0.dp),
+                onClick = {
+                    navController.navigate(Routes.Home.route)
+                    //////////
+                val repos = settingsConnection().currentUser(login, password)
+                repos.enqueue(object : Callback<User> {
+                    override fun onResponse(call: Call<User>, response: Response<User>) {
+                        if (response.isSuccessful) {
+                            val currentUser = response.body()
+                            if (currentUser?.idUser == 0) {
+                                errorMessage = "* Неправильный логин или пароль"
+                            } else if (currentUser?.driver != null) {
+                                navController.navigate(Routes.Home.route)
+                            } else {
+                                errorMessage = "У вас нет прав для этого приложения"
+                            }
+                        }
+                    }
+
+                    override fun onFailure(call: Call<User>, t: Throwable) {
+                        errorMessage = "Ошибка"
+                    }
+                },
+                    )
+            }) {
+                Text("Войти",
+                    fontSize = 28.sp,
+                    )
+            }
         }
     }
 }
