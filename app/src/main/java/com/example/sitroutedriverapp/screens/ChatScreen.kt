ea785d0c3e5e01018d644b1fe9dcdf386b2c7648
Morokenec <149074473+Morokenec@users.noreply.github.com>
2023-12-06 12:05:58 +0300

    IconButtonSend

--- app/src/main/java/com/example/sitroutedriverapp/screens/ChatScreen.kt
+++ app/src/main/java/com/example/sitroutedriverapp/screens/ChatScreen.kt
@@ -1,10 +1,19 @@
  package com.example.sitroutedriverapp.screens
 
 import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Map
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -16,6 +25,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.sitroutedriverapp.Connection
+import com.example.sitroutedriverapp.component.LockScreenOrientation
 import com.example.sitroutedriverapp.models.Message
 import com.example.sitroutedriverapp.settingsConnection
 import retrofit2.Call
@@ -28,29 +38,37 @@ import retrofit2.Response
 fun ChatScreen() {
     var newMessage by rememberSaveable { mutableStateOf("") }
     val messages = mutableListOf<Message>()
+
+    val listCall = settingsConnection().getMessages(Connection.CurrentUser!!.idUser)
+    listCall.enqueue(object : Callback<List<Message>> {
+        override fun onResponse(
+            call: Call<List<Message>>,
+            response: Response<List<Message>>
+        ) {
+            val messagesList = response.body()
+            messages.addAll(messagesList!!)
+        }
+
+        override fun onFailure(call: Call<List<Message>>, t: Throwable) {
+            messages.add(Message(1, "ошибка", null, null, "", null, null))
+        }
+    })
+
     Scaffold(
         bottomBar = {
+            Row {
+
             TextField(value = newMessage, onValueChange = { newMessage = it })
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(Icons.Filled.Send, contentDescription = "")
+            }
+             }
         }) { innerPadding ->
         Column(
             modifier = Modifier
                 .padding(innerPadding),
             verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
-            val listCall = settingsConnection().getMessages(Connection.CurrentUser!!.idUser)
-            listCall.enqueue(object : Callback<List<Message>> {
-                override fun onResponse(
-                    call: Call<List<Message>>,
-                    response: Response<List<Message>>
-                ) {
-                    val messagesList = response.body()
-                    messages.addAll(messagesList!!)
-                }
-
-                override fun onFailure(call: Call<List<Message>>, t: Throwable) {
-                    messages.add(Message(1, "ошибка", null, null, "", null, null))
-                }
-            })
             Column {
                 for (message in messages) {
                     Text(message.value)
