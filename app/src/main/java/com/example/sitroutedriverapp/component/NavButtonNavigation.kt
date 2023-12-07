d2a1b717ba887702f862fd6ff1742be059315d44
Morokenec <149074473+Morokenec@users.noreply.github.com>
2023-12-07 19:03:12 +0300

    ReleaseVersion 1.1.0

--- app/src/main/java/com/example/sitroutedriverapp/component/NavButtonNavigation.kt
+++ app/src/main/java/com/example/sitroutedriverapp/component/NavButtonNavigation.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.icons.filled.DirectionsTransit
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Map
 import androidx.compose.material.icons.filled.Message
+import androidx.compose.material.icons.filled.Route
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -23,24 +24,24 @@ fun ButtonNavigation(navigation: NavHostController, modifier: Modifier = Modifie
     val selectedItem = rememberSaveable { mutableStateOf(Routes.Home.route) }
     NavigationBar {
         NavigationBarItem(
-            icon = { Icon(Icons.Filled.Map , contentDescription = "") },
-            label = { Text("Карта") },
+            icon = { Icon(Icons.Filled.Route , contentDescription = "") },
+            label = { Text("Маршрут") },
             selected = selectedItem.value == Routes.Home.route,
-            onClick = { navigation.navigate(Routes.Home.route)
+            onClick = {if(selectedItem.value != Routes.Home.route)navigation.navigate(Routes.Home.route)
                 selectedItem.value = Routes.Home.route }
         )
         NavigationBarItem(
             icon = { Icon(Icons.Filled.Message, contentDescription = "") },
             label = { Text("Чат") },
             selected = selectedItem.value == Routes.Chat.route,
-            onClick = { navigation.navigate(Routes.Chat.route)
+            onClick = {if(selectedItem.value != Routes.Chat.route) navigation.navigate(Routes.Chat.route)
                 selectedItem.value = Routes.Chat.route}
         )
         NavigationBarItem(
             icon = { Icon(Icons.Filled.DirectionsTransit, contentDescription = "") },
             label = { Text("Состояние") },
             selected = selectedItem.value == Routes.Status.route,
-            onClick = { navigation.navigate(Routes.Status.route)
+            onClick = { if(selectedItem.value != Routes.Status.route)navigation.navigate(Routes.Status.route)
             selectedItem.value = Routes.Status.route}
         )
     }
